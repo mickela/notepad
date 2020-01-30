fecf6b05d87a9e403cd5944adf9f544a27114c3a
Michael <osoba.michael@outlook.com>
2020-01-30 23:46:45 +0100

    Added the pug view engine

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const router = require('./routes/index');
 const db = require('./model/db');
 
 const app = express();
-
+app.use(express.static('public'))
+app.use(express.urlencoded())
 db.authenticate()
 .then(() => {
     console.log('Connection has been established successfully.');
@@ -12,7 +14,8 @@ db.authenticate()
     console.error('Unable to connect to the database:', err);
 });
 
-
+// app.use(cors());
+app.set('view engine', 'pug')
 app.disable('x-powered-by');
 app.listen( process.env.PORT || 3000, ()=> console.log('Server started on port 3000'));
 app.use('/', router);
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,33 @@ const express = require('express');
 const router = express.Router();
 const Note = require('../model/model');
 
-router.use(express.static('public'));
 
 router.get('/', (req, res)=>{
-    res.render('index.html');
+    res.render('index');
 })
+router.post('/newnote/', (req, res)=>{
+    // Create a new user
+    Note.create({ title: req.body.title, body: req.body.body }).then(note => {
+        console.log("Note's auto-generated ID:", note.id);
+    });
+
+    console.log(req.body)
+    res.redirect('/');
+})
+
+router.get('/newnote', (req, res)=>{
+    res.render('newnote');
+})
+
 
 router.get('/notes', (req, res)=>{
     // Find all users
     Note.findAll().then(notes => {
-        const note = JSON.stringify(notes);
-      console.log("All users:", JSON.stringify(notes, null, 4));
-      res.send(note)
+        const note = JSON.stringify(notes, null, 4);
+        console.log("All notes:", JSON.stringify(notes, null, 4));
+        res.send(note)
     });
 })
+
 
 module.exports = router;
